be717d9a2e644063a2d0528e33de3cf724e281c5
sajjad-dev <sajjad.dev2020@gmail.com>
2021-11-02 00:07:21 +0500

    our review

---
+++ app.js
@@ -0,0 +1,13 @@
+ //local revews data 
+ const revews = [
+{
+id:1,
+name:"sajjad ali",
+job:"frontend devaloper",
+img:
+"http://res.cloudinery.com/diqqf3eq2/image/upload/v1586883334/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg",
+text:
+"i'am baby megging twee health goth =1. Bicycle right tumeric chartreaus before they sold out chambray  "
+}
+ ]
+ 
